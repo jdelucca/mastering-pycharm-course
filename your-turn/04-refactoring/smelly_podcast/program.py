80991e907649199b82e9bbd9878ba34b3a45f1f9
jdelucca <76178990+jdelucca@users.noreply.github.com>
2022-11-14 12:25:19 -0600

    clean refactor chapter

--- your-turn/04-refactoring/smelly_podcast/program.py
+++ your-turn/04-refactoring/smelly_podcast/program.py
@@ -8,20 +8,42 @@ episode_data = {}
 
 
 def main():
-    # SHOW THE HEADER
-    print("Welcome to the talk python info downloader.")
-    print()
+    show_header()
+    download_data('https://talkpython.fm/episodes/rss')
 
-    # DOWNLOAD THE EPISODE DATA
-    url = 'https://talkpython.fm/episodes/rss'
+    # GET LATEST SHOW ID
+    latest_show_id, oldest_show_id = get_show_id_range()
 
+    print("Working with total of {} episodes".format(latest_show_id))
+
+    # DISPLAY RESULTS
+    display_results(latest_show_id, oldest_show_id)
+
+
+def display_results(latest_show_id, oldest_show_id):
+    start = oldest_show_id
+    end = latest_show_id + 1
+    for show_id in range(start, end):
+        # GET EPISODE
+        info = get_episode(show_id)
+        print("{}. {}".format(info.show_id, info.title))
+
+
+def get_episode(show_id):
+    return episode_data.get(show_id)
+
+
+def get_show_id_range():
+    latest_show_id = max(episode_data.keys())
+    oldest_show_id = min(episode_data.keys())
+    return latest_show_id, oldest_show_id
+
+
+def download_data(url):
     resp = requests.get(url)
     resp.raise_for_status()
-
     dom = ElementTree.fromstring(resp.text)
-
     episode_count = len(dom.findall('channel/item'))
-
     for idx, item in enumerate(dom.findall('channel/item')):
         episode = Episode(
             item.find('title').text,
@@ -31,20 +53,10 @@ def main():
         )
         episode_data[episode.show_id] = episode
 
-    # GET LATEST SHOW ID
-    latest_show_id = max(episode_data.keys())
-    oldest_show_id = min(episode_data.keys())
 
-    print("Working with total of {} episodes".format(latest_show_id))
-
-    # DISPLAY RESULTS
-    start = oldest_show_id
-    end = latest_show_id + 1
-
-    for show_id in range(start, end):
-        # GET EPISODE
-        info = episode_data.get(show_id)
-        print("{}. {}".format(info.show_id, info.title))
+def show_header():
+    print("Welcome to the talk python info downloader.")
+    print()
 
 
 if __name__ == '__main__':
